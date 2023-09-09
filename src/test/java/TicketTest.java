6c3911cf8de5bb69d93e26862a80d6d24978ba12
Anastasia <90128050+SantAlice@users.noreply.github.com>
2023-09-09 16:32:09 +0300

    Created classes and test

---
+++ src/test/java/TicketTest.java
@@ -0,0 +1,88 @@
+import org.junit.Test;
+
+public class TicketTest {
+
+    @Test
+
+    public void sortingTest() {
+        AviaSouls manager = new AviaSouls();
+        Ticket ticket1 = new Ticket("MSK","EKB",210,9,13);
+        Ticket ticket2 = new Ticket("NVS","MSK",320,11,14);
+        Ticket ticket3 = new Ticket("NYK","MSK",510,4,10);
+        Ticket ticket4 = new Ticket("KRG","SPB",400,3,6);
+        Ticket ticket5 = new Ticket("AST","EKB",410,9,11);
+        Ticket ticket6 = new Ticket("MSK","EKB",210,12,13);
+        Ticket ticket7 = new Ticket("AST","EKB",400,10,15);
+        Ticket ticket8 = new Ticket("NYK","MSK",510,4,10);
+        Ticket ticket9 = new Ticket("AST","EKB",410,10,12);
+
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+        manager.add(ticket7);
+        manager.add(ticket8);
+        manager.add(ticket9);
+
+        Ticket[] actual = manager.search("AST", "EKB");
+        Ticket[] expected = {ticket5, ticket9, ticket8};
+    }
+
+    @Test
+
+    public void sortingTestNotExisting() {
+        AviaSouls manager = new AviaSouls();
+        Ticket ticket1 = new Ticket("MSK","EKB",210,9,13);
+        Ticket ticket2 = new Ticket("NVS","MSK",320,11,14);
+        Ticket ticket3 = new Ticket("NYK","MSK",510,4,10);
+        Ticket ticket4 = new Ticket("KRG","SPB",400,3,6);
+        Ticket ticket5 = new Ticket("AST","EKB",410,9,11);
+        Ticket ticket6 = new Ticket("MSK","EKB",210,12,13);
+        Ticket ticket7 = new Ticket("AST","EKB",400,10,15);
+        Ticket ticket8 = new Ticket("NYK","MSK",510,4,10);
+        Ticket ticket9 = new Ticket("AST","EKB",410,10,12);
+
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+        manager.add(ticket7);
+        manager.add(ticket8);
+        manager.add(ticket9);
+
+        Ticket[] actual = manager.search("AST", "NYK");
+        Ticket[] expected = { };
+    }
+
+    @Test
+
+    public void sortingTestBoundary() {
+        AviaSouls manager = new AviaSouls();
+        Ticket ticket1 = new Ticket("MSK","EKB",210,9,13);
+        Ticket ticket2 = new Ticket("NVS","MSK",320,11,14);
+        Ticket ticket3 = new Ticket("NYK","MSK",510,4,10);
+        Ticket ticket4 = new Ticket("KRG","SPB",400,3,6);
+        Ticket ticket5 = new Ticket("AST","EKB",410,9,11);
+        Ticket ticket6 = new Ticket("MSK","EKB",210,12,13);
+        Ticket ticket7 = new Ticket("AST","EKB",400,10,15);
+        Ticket ticket8 = new Ticket("NYK","MSK",510,4,10);
+        Ticket ticket9 = new Ticket("AST","EKB",410,10,12);
+
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+        manager.add(ticket7);
+        manager.add(ticket8);
+        manager.add(ticket9);
+
+        Ticket[] actual = manager.search("KRG", "SPB");
+        Ticket[] expected = { };
+    }
+}
